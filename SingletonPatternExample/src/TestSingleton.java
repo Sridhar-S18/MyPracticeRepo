f71ce5179d412aaa82e409d3ec450154718dd964
Sridhar S <thenameissridhar1709@gmail.com>
2025-06-21 12:21:41 +0530

    Delete SingletonPatternExample directory

--- SingletonPatternExample/src/TestSingleton.java
+++
@@ -1,9 +0,0 @@
-public class TestSingleton {
-    public static void main(String[] args) {
-        Logger l1 = Logger.getInstance();
-        Logger l2 = Logger.getInstance();
-
-        //Test if both instance are same
-        System.out.println(l1 == l2);
-    }
-}
