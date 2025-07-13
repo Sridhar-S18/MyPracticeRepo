09197133f9aeb8f5c8279d5cd7a81cded03db61e
Sridhar S <thenameissridhar1709@gmail.com>
2025-07-13 21:40:09 +0530

    Add files via upload

---
+++ Week 4/mandatory_HandsOn_2/src/main/java/com/example/mandatory_HandsOn_2/MandatoryHandsOn2Application.java
@@ -0,0 +1,25 @@
+package com.example.mandatory_HandsOn_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class MandatoryHandsOn2Application {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MandatoryHandsOn2Application.class);
+
+	public static void main(String[] args) {
+		displayCountry();
+	}
+
+	public static void displayCountry() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = (Country) context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country.toString());
+	}
+
+}
---
+++ Week 4/mandatory_HandsOn_2/src/test/java/com/example/mandatory_HandsOn_2/MandatoryHandsOn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mandatory_HandsOn_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandatoryHandsOn2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
