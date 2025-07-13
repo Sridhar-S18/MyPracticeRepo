e45992f2643f6fb6d95df53f86ee615ea971fbff
Sridhar S <thenameissridhar1709@gmail.com>
2025-07-13 20:12:34 +0530

    Add files via upload

---
+++ Week 4/Hands_on_1/src/main/java/com/example/Hands_on_1/HandsOn1Application.java
@@ -0,0 +1,13 @@
+package com.example.Hands_on_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOn1Application.class, args);
+	}
+
+}
---
+++ Week 4/Hands_on_1/src/test/java/com/example/Hands_on_1/HandsOn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hands_on_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOn1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
